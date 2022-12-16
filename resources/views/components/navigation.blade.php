a4e3d3f40b09fe508e9aa751e75f8c860bdf63e8
Abdul Hafidz A <aditans88@gmail.com>
2022-12-16 13:12:30 +0700

    update

---
+++ resources/views/components/navigation.blade.php
@@ -0,0 +1,20 @@
+<li class="{{ ! $navigation->children->isEmpty() ? 'treeview': '' }}">
+    <a href="{{ $navigation->route }}">
+        @if($navigation->icon)
+            <i class="{{ $navigation->icon }}"></i>
+        @endif
+        <span>{{ $navigation->name }}</span>
+        @if(! $navigation->children->isEmpty())
+        <span class="pull-right-container">
+            <i class="fa fa-angle-left pull-right"></i>
+        </span>
+        @endif
+    </a>
+    @if(! $navigation->children->isEmpty())
+    <ul class="treeview-menu">
+        @foreach($navigation->children as $child)
+            <x-navigation :navigation="$child"></x-navigation>
+        @endforeach
+    </ul>
+    @endif
+</li>
--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -7,22 +7,12 @@
                     <i class="fa fa-dashboard"></i> <span>Dashboard</span>
                 </a>
             </li>
-            <li class="treeview">
-                <a href="#">
-                    <i class="fa fa-files-o"></i>
-                    <span>Layout Options</span>
-                    <span class="pull-right-container">
-                        <i class="fa fa-angle-left pull-right"></i>
-                    </span>
-                </a>
-                <ul class="treeview-menu">
-                    <li><a href="pages/layout/top-nav.html"><i class="fa fa-circle-o"></i> Top Navigation</a></li>
-                    <li><a href="pages/layout/boxed.html"><i class="fa fa-circle-o"></i> Boxed</a></li>
-                    <li><a href="pages/layout/fixed.html"><i class="fa fa-circle-o"></i> Fixed</a></li>
-                    <li><a href="pages/layout/collapsed-sidebar.html"><i class="fa fa-circle-o"></i> Collapsed Sidebar</a></li>
-                </ul>
-            </li>
-            <li class="active">
+
+            @foreach(\App\Models\Navigation::tree() as $nav)
+                <x-navigation :navigation="$nav"></x-navigation>
+            @endforeach
+            
+            <li>
                 <a href="#">
                     <i class="fa fa-info-circle"></i> <span>About</span>
                 </a>
